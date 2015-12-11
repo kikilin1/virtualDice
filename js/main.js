47d9d4c3d6f52980ece96ba58fb1dfdddd39e99f
kikilin1 <kikiqanlin@gmail.com>
2015-12-10 21:06:17 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 var img = document.getElementById("diceimg1");
 
 img.onclick = function(){
-var face = Math.round(Math.random()*5+1)
-document.getElementById("roll").innerHTML=face;
+    var face = Math.round(Math.random()*6);
     
-console.log(face)
+    if(img.src.includes("frame1")){
+        img.src="img/dodecahedron.gif";
+        //document.getElementById("roll").innerHTML=face;
+        setTimeout(function(){
+            //change image back to stationary one (png)
+            img.src = "img/dodecahedron-frame1.png";
+            document.getElementById("roll").innerHTML=face;
+        },2000);
+    }
+    
+    
+
 };
+
